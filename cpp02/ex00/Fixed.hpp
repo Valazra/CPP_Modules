521525af105e3ddabd78bbb7cae51c0c3873cb53
Azra <valentin.azra@gmail.com>
2022-03-10 00:33:51 +0100

    cpp02

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 15:39:53 by user42            #+#    #+#             */
+/*   Updated: 2022/03/09 15:46:18 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	public :
+		Fixed(void);
+		~Fixed(void);
+	private :
+		int			nb;
+		static const int	nb_bits;
+};
+
+#endif

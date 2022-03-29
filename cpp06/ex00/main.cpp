59b27a468b857e6d7cf8ef75d9ec0388b4351409
Valentin AZRA <vazra@e2r3p9.clusters.42paris.fr>
2022-03-29 17:22:53 +0200

    yes

---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream.h>
+
+int	main(int ac, char **av)
+{
+	int	type;
+
+	type = -1;
+	if (ac != 2)
+	{
+		std::cout << "Bad number of arguments" << std::endl;
+		return (-1);
+	}
+	type = getType(av[1]);
+	if (type == 0)
+	{
+		std::cout << "Bad type" << std::endl;
+		return (-1);
+	}
+	else if (type == 1) //char
+	{
+		//deja en char *
+		treat(av[1]);
+	}
+	else if (type == 2) // int
+	{
+		int	type_int;
+		type_int = convert_int(av[1]);
+		treat(type_int);
+	}
+	else if (type == 3) //float
+	{
+		float	type_float;
+		type_float = convert_float(av[1]);
+		treat(type_float);
+	}
+	else if (type == 4) // double
+	{
+		double	type_double;
+		type_double = convert_double(av[1]);
+		treat(type_double);
+	}
+	return (0);
+}

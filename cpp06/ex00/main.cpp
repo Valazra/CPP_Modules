817ee8599a3aad3d5f154dd6f8eb3f005dfa32ae
Valentin AZRA <vazra@e1r5p4.clusters.42paris.fr>
2022-05-16 14:15:02 +0200

    on ajuste

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -3,13 +3,20 @@
 
 int	main(int ac, char **av)
 {
-//penser à un arg vide ""
 	if (ac != 2)
 	{
 		std::cout << "Bad number of arguments" << std::endl;
 		return (-1);
 	}
-	Convert av1(av[1]);
-	av1.convertType();
+	else
+	{
+		if (av[1][0] == '\0')
+		{
+			std::cout << "Arg must not be empty" << std::endl;
+			return (0);
+		}
+		Convert av1(av[1]);
+		av1.convertType();
+	}
 	return (0);
 }

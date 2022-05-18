598b47464af8184bc1af5cd90e7645fb89803fac
Valentin AZRA <vazra@e2r13p4.clusters.42paris.fr>
2022-05-18 16:36:10 +0200

    debut ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,13 +2,29 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include <stdexcept>
 
 template < typename T >
 class Array
 {
 	public :
-		Array(void);
-		Array(unsigned int n);
+		Array(void) : _array(NULL), _size(0){};
+		Array(unsigned int n) : _size(n){};
+		Array(Array const & src);
+		~Array(void);
+
+		Array & operator=(Array const & rhs);
+
+		unsigned int size(void);
+
+		class BadIndexException : public std::exception
+		{
+			public :
+				virtual const char* what(void) const throw()
+				{
+					return ("Array : Bad Index");
+				}
+		}
 
 	private :
 		T* _array;
